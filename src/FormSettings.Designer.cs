e39eae1f45e335d9da91b07ffa48bf5e15c656de
Marco Graf <grafmar78@gmail.com>
2022-02-23 07:24:04 +0100

    App build-up

---
+++ src/FormSettings.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace PresenceBridge
+{
+	partial class FormSettings
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.SystemTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+			this.btnAvailable = new System.Windows.Forms.Button();
+			this.labelAvailable = new System.Windows.Forms.Label();
+			this.labelBusy = new System.Windows.Forms.Label();
+			this.btnBusy = new System.Windows.Forms.Button();
+			this.labelAway = new System.Windows.Forms.Label();
+			this.btnAway = new System.Windows.Forms.Button();
+			this.labelDoNotDisturb = new System.Windows.Forms.Label();
+			this.btnDoNotDisturb = new System.Windows.Forms.Button();
+			this.labelOffline = new System.Windows.Forms.Label();
+			this.btnOffline = new System.Windows.Forms.Button();
+			this.trackBarBrightness = new System.Windows.Forms.TrackBar();
+			this.labelBrightness = new System.Windows.Forms.Label();
+			this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+			this.labelSerialPort = new System.Windows.Forms.Label();
+			this.labelTeamsConnection = new System.Windows.Forms.Label();
+			this.pictureBoxFoto = new System.Windows.Forms.PictureBox();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.btnReload = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// SystemTrayIcon
+			// 
+			this.SystemTrayIcon.Text = "SystemTrayIcon";
+			this.SystemTrayIcon.Visible = true;
+			this.SystemTrayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.SystemTrayIcon_MouseClick);
+			// 
+			// btnAvailable
+			// 
+			this.btnAvailable.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAvailable.Location = new System.Drawing.Point(12, 12);
+			this.btnAvailable.Name = "btnAvailable";
+			this.btnAvailable.Size = new System.Drawing.Size(20, 20);
+			this.btnAvailable.TabIndex = 0;
+			this.btnAvailable.UseVisualStyleBackColor = true;
+			this.btnAvailable.Click += new System.EventHandler(this.selectColor);
+			// 
+			// labelAvailable
+			// 
+			this.labelAvailable.AutoSize = true;
+			this.labelAvailable.Location = new System.Drawing.Point(38, 16);
+			this.labelAvailable.Name = "labelAvailable";
+			this.labelAvailable.Size = new System.Drawing.Size(50, 13);
+			this.labelAvailable.TabIndex = 1;
+			this.labelAvailable.Text = "Available";
+			this.labelAvailable.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// labelBusy
+			// 
+			this.labelBusy.AutoSize = true;
+			this.labelBusy.Location = new System.Drawing.Point(38, 42);
+			this.labelBusy.Name = "labelBusy";
+			this.labelBusy.Size = new System.Drawing.Size(30, 13);
+			this.labelBusy.TabIndex = 3;
+			this.labelBusy.Text = "Busy";
+			this.labelBusy.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnBusy
+			// 
+			this.btnBusy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnBusy.Location = new System.Drawing.Point(12, 38);
+			this.btnBusy.Name = "btnBusy";
+			this.btnBusy.Size = new System.Drawing.Size(20, 20);
+			this.btnBusy.TabIndex = 2;
+			this.btnBusy.UseVisualStyleBackColor = true;
+			this.btnBusy.Click += new System.EventHandler(this.selectColor);
+			// 
+			// labelAway
+			// 
+			this.labelAway.AutoSize = true;
+			this.labelAway.Location = new System.Drawing.Point(38, 68);
+			this.labelAway.Name = "labelAway";
+			this.labelAway.Size = new System.Drawing.Size(33, 13);
+			this.labelAway.TabIndex = 5;
+			this.labelAway.Text = "Away";
+			this.labelAway.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnAway
+			// 
+			this.btnAway.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnAway.Location = new System.Drawing.Point(12, 64);
+			this.btnAway.Name = "btnAway";
+			this.btnAway.Size = new System.Drawing.Size(20, 20);
+			this.btnAway.TabIndex = 4;
+			this.btnAway.UseVisualStyleBackColor = true;
+			this.btnAway.Click += new System.EventHandler(this.selectColor);
+			// 
+			// labelDoNotDisturb
+			// 
+			this.labelDoNotDisturb.AutoSize = true;
+			this.labelDoNotDisturb.Location = new System.Drawing.Point(38, 94);
+			this.labelDoNotDisturb.Name = "labelDoNotDisturb";
+			this.labelDoNotDisturb.Size = new System.Drawing.Size(71, 13);
+			this.labelDoNotDisturb.TabIndex = 7;
+			this.labelDoNotDisturb.Text = "DoNotDisturb";
+			this.labelDoNotDisturb.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnDoNotDisturb
+			// 
+			this.btnDoNotDisturb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnDoNotDisturb.Location = new System.Drawing.Point(12, 90);
+			this.btnDoNotDisturb.Name = "btnDoNotDisturb";
+			this.btnDoNotDisturb.Size = new System.Drawing.Size(20, 20);
+			this.btnDoNotDisturb.TabIndex = 6;
+			this.btnDoNotDisturb.UseVisualStyleBackColor = true;
+			this.btnDoNotDisturb.Click += new System.EventHandler(this.selectColor);
+			// 
+			// labelOffline
+			// 
+			this.labelOffline.AutoSize = true;
+			this.labelOffline.Location = new System.Drawing.Point(38, 120);
+			this.labelOffline.Name = "labelOffline";
+			this.labelOffline.Size = new System.Drawing.Size(37, 13);
+			this.labelOffline.TabIndex = 9;
+			this.labelOffline.Text = "Offline";
+			this.labelOffline.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// btnOffline
+			// 
+			this.btnOffline.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnOffline.Location = new System.Drawing.Point(12, 116);
+			this.btnOffline.Name = "btnOffline";
+			this.btnOffline.Size = new System.Drawing.Size(20, 20);
+			this.btnOffline.TabIndex = 8;
+			this.btnOffline.UseVisualStyleBackColor = true;
+			this.btnOffline.Click += new System.EventHandler(this.selectColor);
+			// 
+			// trackBarBrightness
+			// 
+			this.trackBarBrightness.LargeChange = 10;
+			this.trackBarBrightness.Location = new System.Drawing.Point(12, 186);
+			this.trackBarBrightness.Maximum = 100;
+			this.trackBarBrightness.Name = "trackBarBrightness";
+			this.trackBarBrightness.Size = new System.Drawing.Size(260, 45);
+			this.trackBarBrightness.TabIndex = 10;
+			this.trackBarBrightness.TickFrequency = 10;
+			this.trackBarBrightness.Value = 100;
+			this.trackBarBrightness.Scroll += new System.EventHandler(this.trackBarBrightness_Scroll);
+			// 
+			// labelBrightness
+			// 
+			this.labelBrightness.AutoSize = true;
+			this.labelBrightness.Location = new System.Drawing.Point(12, 170);
+			this.labelBrightness.Name = "labelBrightness";
+			this.labelBrightness.Size = new System.Drawing.Size(56, 13);
+			this.labelBrightness.TabIndex = 11;
+			this.labelBrightness.Text = "Brightness";
+			this.labelBrightness.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// comboBoxSerialPort
+			// 
+			this.comboBoxSerialPort.FormattingEnabled = true;
+			this.comboBoxSerialPort.Location = new System.Drawing.Point(12, 250);
+			this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+			this.comboBoxSerialPort.Size = new System.Drawing.Size(260, 21);
+			this.comboBoxSerialPort.TabIndex = 12;
+			this.comboBoxSerialPort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSerialPort_SelectedIndexChanged);
+			this.comboBoxSerialPort.Click += new System.EventHandler(this.comboBoxSerialPort_Click);
+			// 
+			// labelSerialPort
+			// 
+			this.labelSerialPort.AutoSize = true;
+			this.labelSerialPort.Location = new System.Drawing.Point(12, 234);
+			this.labelSerialPort.Name = "labelSerialPort";
+			this.labelSerialPort.Size = new System.Drawing.Size(133, 13);
+			this.labelSerialPort.TabIndex = 13;
+			this.labelSerialPort.Text = "PresenceLight\'s Serial Port";
+			this.labelSerialPort.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// labelTeamsConnection
+			// 
+			this.labelTeamsConnection.AutoSize = true;
+			this.labelTeamsConnection.Location = new System.Drawing.Point(12, 329);
+			this.labelTeamsConnection.Name = "labelTeamsConnection";
+			this.labelTeamsConnection.Size = new System.Drawing.Size(96, 13);
+			this.labelTeamsConnection.TabIndex = 14;
+			this.labelTeamsConnection.Text = "Teams Connection";
+			this.labelTeamsConnection.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// pictureBoxFoto
+			// 
+			this.pictureBoxFoto.Location = new System.Drawing.Point(69, 345);
+			this.pictureBoxFoto.Name = "pictureBoxFoto";
+			this.pictureBoxFoto.Size = new System.Drawing.Size(150, 150);
+			this.pictureBoxFoto.TabIndex = 15;
+			this.pictureBoxFoto.TabStop = false;
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.Location = new System.Drawing.Point(103, 528);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(75, 23);
+			this.btnLogin.TabIndex = 16;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = true;
+			this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+			// 
+			// btnReload
+			// 
+			this.btnReload.Location = new System.Drawing.Point(63, 277);
+			this.btnReload.Name = "btnReload";
+			this.btnReload.Size = new System.Drawing.Size(75, 23);
+			this.btnReload.TabIndex = 17;
+			this.btnReload.Text = "Reload";
+			this.btnReload.UseVisualStyleBackColor = true;
+			this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(144, 277);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 18;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+			// 
+			// FormSettings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 563);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.btnReload);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.pictureBoxFoto);
+			this.Controls.Add(this.labelTeamsConnection);
+			this.Controls.Add(this.labelSerialPort);
+			this.Controls.Add(this.comboBoxSerialPort);
+			this.Controls.Add(this.labelBrightness);
+			this.Controls.Add(this.trackBarBrightness);
+			this.Controls.Add(this.labelOffline);
+			this.Controls.Add(this.btnOffline);
+			this.Controls.Add(this.labelDoNotDisturb);
+			this.Controls.Add(this.btnDoNotDisturb);
+			this.Controls.Add(this.labelAway);
+			this.Controls.Add(this.btnAway);
+			this.Controls.Add(this.labelBusy);
+			this.Controls.Add(this.btnBusy);
+			this.Controls.Add(this.labelAvailable);
+			this.Controls.Add(this.btnAvailable);
+			this.Name = "FormSettings";
+			this.Text = "PresenceBridge";
+			((System.ComponentModel.ISupportInitialize)(this.trackBarBrightness)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon SystemTrayIcon;
+		private System.Windows.Forms.Button btnAvailable;
+		private System.Windows.Forms.Label labelAvailable;
+		private System.Windows.Forms.Label labelBusy;
+		private System.Windows.Forms.Button btnBusy;
+		private System.Windows.Forms.Label labelAway;
+		private System.Windows.Forms.Button btnAway;
+		private System.Windows.Forms.Label labelDoNotDisturb;
+		private System.Windows.Forms.Button btnDoNotDisturb;
+		private System.Windows.Forms.Label labelOffline;
+		private System.Windows.Forms.Button btnOffline;
+		private System.Windows.Forms.TrackBar trackBarBrightness;
+		private System.Windows.Forms.Label labelBrightness;
+		private System.Windows.Forms.ComboBox comboBoxSerialPort;
+		private System.Windows.Forms.Label labelSerialPort;
+		private System.Windows.Forms.Label labelTeamsConnection;
+		private System.Windows.Forms.PictureBox pictureBoxFoto;
+		private System.Windows.Forms.Button btnLogin;
+		private System.Windows.Forms.Button btnReload;
+		private System.Windows.Forms.Button btnSave;
+	}
+}
+
---
+++ src/FormSettings.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using Microsoft.Graph; // NuGet Package
+using Azure.Identity;  // NuGet Package
+
+
+namespace PresenceBridge
+{
+	public partial class FormSettings : Form
+	{
+		public FormSettings()
+		{
+			InitializeComponent();
+
+			// initialize for SystemTrayApp
+
+			this.CenterToScreen();
+
+			// Set application icon and system tray icon
+			this.Icon = Properties.Resources.icon_mix_UPh_icon;
+			this.SystemTrayIcon.Icon = Properties.Resources.trayicon_512_Olk_icon;
+
+			// Change the Text property to the name of your application
+			this.SystemTrayIcon.Text = "PresenceBridge";
+			this.SystemTrayIcon.Visible = true;
+
+			// Modify the right-click menu of your system tray icon here
+			ContextMenu menu = new ContextMenu();
+			menu.MenuItems.Add("Settings", ContextMenuSettings);
+			menu.MenuItems.Add("Exit", ContextMenuExit);
+			this.SystemTrayIcon.ContextMenu = menu;
+
+			this.Resize += WindowResize;
+			this.FormClosing += WindowClosing;
+
+			//updateColorDescriptions();
+			applySettings();
+			getSerialPorts();
+		}
+
+		private void ContextMenuSettings(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+			this.Show();
+			this.WindowState = FormWindowState.Normal;
+		}
+
+		private void ContextMenuExit(object sender, EventArgs e)
+		{
+			this.SystemTrayIcon.Visible = false;
+			System.Windows.Forms.Application.Exit();
+			Environment.Exit(0);
+		}
+
+		private void WindowResize(object sender, EventArgs e)
+		{
+			if (this.WindowState == FormWindowState.Minimized)
+			{
+				this.Hide();
+			}
+		}
+
+		private void WindowClosing(object sender, FormClosingEventArgs e)
+		{
+			e.Cancel = true;
+			this.Hide();
+		}
+
+		private void selectColor(object sender, EventArgs e)
+		{
+			ColorDialog MyDialog = new ColorDialog();
+			MyDialog.AllowFullOpen = true;
+			MyDialog.FullOpen = true;
+			MyDialog.CustomColors = new int[] { 0x0000FF, 0x00FF00, 0x0064FF, 0x9600FF, 0x330000 };
+			MyDialog.Color = (sender as Button).BackColor;
+
+			// Update the text box color if the user clicks OK 
+			if (MyDialog.ShowDialog() == DialogResult.OK)
+				(sender as Button).BackColor = MyDialog.Color;
+
+			//if ((comboBox1.SelectedItem != null) && (comboBox1.SelectedItem.ToString() != ""))
+			//{
+			//	SerialPort _serialPort = new SerialPort(comboBox1.SelectedItem.ToString(), 115200, Parity.None, 8, StopBits.One);
+			//	_serialPort.Handshake = Handshake.None;
+			//	try
+			//	{
+			//		if (!(_serialPort.IsOpen))
+			//		{
+			//			_serialPort.Open();
+
+
+			//			_serialPort.Write("rgb:" + MyDialog.Color.R + "," + MyDialog.Color.G + "," + MyDialog.Color.B + "\r\n");
+			//			_serialPort.Close();
+			//		}
+			//	}
+			//	catch (Exception ex)
+			//	{
+			//		MessageBox.Show("Error opening/writing to serial port :: " + ex.Message, "Error!");
+			//	}
+			//	updateColorDescriptions();
+			//}
+			saveSettings();
+
+		}
+
+		private void trackBarBrightness_Scroll(object sender, EventArgs e)
+		{
+			labelBrightness.Text = "Brightness " + trackBarBrightness.Value + "%";
+			saveSettings();
+		}
+
+		private int Clamp(int value, int min, int max)
+		{
+			return Math.Min(Math.Max(value, min), max);
+		}
+
+		private void applySettings()
+		{
+			btnAvailable.BackColor		= System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorAvailable);
+			btnBusy.BackColor			= System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorBusy);
+			btnAway.BackColor			= System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorAway);
+			btnDoNotDisturb.BackColor	= System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorDoNotDisturb);
+			btnOffline.BackColor		= System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorOffline);
+			trackBarBrightness.Value	= Clamp(Properties.Settings.Default.Brightness, 0, 100);
+			labelBrightness.Text		= "Brightness " + trackBarBrightness.Value + "%";
+			comboBoxSerialPort.Text		= Properties.Settings.Default.SerialPort;
+		}
+
+		private void saveSettings()
+		{
+			Properties.Settings.Default.ColorAvailable		= System.Drawing.ColorTranslator.ToHtml(btnAvailable.BackColor);
+			Properties.Settings.Default.ColorBusy			= System.Drawing.ColorTranslator.ToHtml(btnBusy.BackColor);
+			Properties.Settings.Default.ColorAway			= System.Drawing.ColorTranslator.ToHtml(btnAway.BackColor);
+			Properties.Settings.Default.ColorDoNotDisturb	= System.Drawing.ColorTranslator.ToHtml(btnDoNotDisturb.BackColor);
+			Properties.Settings.Default.ColorOffline		= System.Drawing.ColorTranslator.ToHtml(btnOffline.BackColor);
+			Properties.Settings.Default.Brightness			= trackBarBrightness.Value;
+			Properties.Settings.Default.SerialPort			= comboBoxSerialPort.Text;
+		}
+
+		private void getSerialPorts()
+		{
+			string[] ports = SerialPort.GetPortNames();
+			comboBoxSerialPort.Items.Clear();
+			foreach (string s in SerialPort.GetPortNames())
+			{
+				comboBoxSerialPort.Items.Add(s);
+			}
+		}
+		private void setPresenceColor(Color color)
+		{
+			// Alter System Tray Icon color
+			Bitmap bmp = SystemTrayIcon.Icon.ToBitmap();
+			using (Graphics gr = Graphics.FromImage(bmp))
+			{
+				int size = bmp.Width;
+				SolidBrush brush = new SolidBrush(color);
+				gr.FillRectangle(brush, 6*size/32, 2*size/32, 19*size/32, 18*size/32);
+			}
+			IntPtr Hicon = bmp.GetHicon();
+			SystemTrayIcon.Icon = Icon.FromHandle(Hicon);
+
+			// Show Color in Profile Photo
+			if (pictureBoxFoto.Image != null)
+			{
+				var Foto = pictureBoxFoto.Image;
+				using (Graphics gr = Graphics.FromImage(Foto))
+				{
+					SolidBrush brush = new SolidBrush(color);
+					gr.FillEllipse(brush, pictureBoxFoto.Width - 40, pictureBoxFoto.Height - 40, 40, 40);
+				}
+				pictureBoxFoto.Image = Foto;
+			}
+		}
+
+		private void comboBoxSerialPort_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			saveSettings();
+		}
+
+		private void comboBoxSerialPort_Click(object sender, EventArgs e)
+		{
+			getSerialPorts();
+		}
+
+		private void btnReload_Click(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.Reload();
+			applySettings();
+		}
+
+		private void btnSave_Click(object sender, EventArgs e)
+		{
+			Properties.Settings.Default.Save();
+		}
+
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{
+			if (btnLogin.Text == "Logout")
+			{
+				doLogout();
+				btnLogin.Text = "Login";
+			}
+			else
+			{
+				doLogin();
+				btnLogin.Text = "Logout";
+			}
+		}
+
+		private async void doLogout()
+		{ 
+		}
+
+		private async void doLogin()
+		{
+			var scopes = new[] { "User.Read", "Presence.Read" };
+			// multi-tenant apps can use "common",
+			// single-tenant apps must use the tenant id from the azure portal
+			var tenantId = "common";
+
+			// value from app registration
+			//var clientId = Properties.Settings.Default.ClientId;
+			var clientId = "3bd2647e-821e-48dd-a4b3-158e87fd7945";
+
+			// using Azure.Identity;
+			var options = new InteractiveBrowserCredentialOptions
+			{
+				TenantId = tenantId,
+				ClientId = clientId,
+				AuthorityHost = AzureAuthorityHosts.AzurePublicCloud,
+				// MUST be http://localhost or http://localhost:PORT
+				// See https://github.com/AzureAD/microsoft-authentication-library-for-dotnet/wiki/System-Browser-on-.Net-Core
+				RedirectUri = new Uri("http://localhost"),
+			};
+
+			// https://docs.microsoft.com/dotnet/api/azure.identity.interactivebrowsercredential
+			var interactiveCredential = new InteractiveBrowserCredential(options);
+
+
+			var graphClient = new GraphServiceClient(interactiveCredential, scopes);
+
+			//var user = await graphClient.Me.Request().GetAsync();
+
+			// Get account photo
+			System.IO.Stream photo = await graphClient.Me.Photo.Content.Request().GetAsync();
+			if (photo != null)
+			{
+				System.IO.MemoryStream ms = new System.IO.MemoryStream();
+				photo.CopyTo(ms);
+				Bitmap bmp = new Bitmap(System.Drawing.Image.FromStream(ms), pictureBoxFoto.Size);
+				pictureBoxFoto.Image = ClipToCircle(bmp);
+			}
+			else
+			{
+				// create image for Profile Photo if none is set
+				pictureBoxFoto.Image = new Bitmap(pictureBoxFoto.Width, pictureBoxFoto.Height);
+			}
+
+			Presence presence = await graphClient.Me.Presence.Request().GetAsync();
+			setPresenceColor(getColorFromPresence(presence));
+
+			//interactiveCredential.Authenticate();
+
+			//interactiveCredential.AuthenticationRecord
+			//if (interactiveCredential.Record.Username != null)
+			//{
+
+			//}
+		}
+
+		private Color getColorFromPresence(Presence presence)
+		{
+			Color retVal= Color.Black;
+			switch (presence.Availability)
+			{
+				case "Available":
+				case "AvailableIdle":
+					retVal = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorAvailable);
+					break;
+				case "Busy":
+				case "BusyIdle":
+					retVal = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorBusy);
+					break;
+				case "DoNotDisturb":
+					retVal = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorDoNotDisturb);
+					break;
+				case "Away":
+				case "BeRightBack":
+					retVal = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorAway);
+					break;
+				case "Offline":
+				case "PresenceUnknown":
+				default:
+					retVal = System.Drawing.ColorTranslator.FromHtml(Properties.Settings.Default.ColorOffline);
+					break;
+			}
+			return retVal;
+		}
+
+		public Bitmap ClipToCircle(Bitmap bm)
+		{
+			Bitmap bt = new Bitmap(bm.Width, bm.Height);
+			Graphics g = Graphics.FromImage(bt);
+			GraphicsPath gp = new GraphicsPath();
+			gp.AddEllipse(10, 10, bm.Width - 20, bm.Height - 20);
+			g.Clear(Color.Magenta);
+			g.SetClip(gp);
+			g.DrawImage(bm, new
+			Rectangle(0, 0, bm.Width, bm.Height), 0, 0, bm.Width, bm.Height, GraphicsUnit.Pixel);
+			g.Dispose();
+			bt.MakeTransparent(Color.Magenta);
+			return bt;
+		}
+		            
+		private void SystemTrayIcon_MouseClick(object sender, MouseEventArgs e)
+		{
+			ContextMenuSettings(sender, e);
+		}
+	}
+}
